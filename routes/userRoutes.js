8b488e5b382573456add604ad80c21ef256d5d16
ravi-techprime <rajravi78600@gmail.com>
2025-03-11 14:29:24 +0530

    final commit backend server

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 const authMiddleware = require("../utill/authMiddleware");
 
+
 router.post("/singup", userController.createuser);
 router.post("/login", userController.loginuser);
 router.post("/forget-password", userController.forgetPassword);
@@ -13,5 +14,6 @@ router.get("/tasks", authMiddleware, userController.getAllTasks);
 router.get("/tasks/:id", authMiddleware, userController.getSpecificTask);
 router.put("/tasks/:id", authMiddleware, userController.updateTask);
 router.delete("/tasks/:id", authMiddleware, userController.deleteTask);
+
 
 module.exports = router;
